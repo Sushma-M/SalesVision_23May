3a63b9bcbc9343dd27e24befbc113c489b4514b8
Sushma Wavemaker <sushma.maddirala@wavemaker.com>
2018-05-23 10:25:27 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,12 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "flyer",
+  "defaultLanguage" : "en",
+  "displayName" : "SalesVision_23May",
+  "homePage" : "Main",
+  "name" : "SalesVision_23May",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
